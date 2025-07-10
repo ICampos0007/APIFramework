fa8c8a2836ca79bd4c78065dcda48f276d1e3dd3
Irvin <irvincampos76@gmail.com>
2025-07-10 15:49:43 -0700

    Excellent cucumber html reporting added

--- src/test/java/org/IrvinCampos/cucumber/Options/TestRunner.java
+++ src/test/java/org/IrvinCampos/cucumber/Options/TestRunner.java
@@ -3,7 +3,7 @@ package org.IrvinCampos.cucumber.Options;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/org/IrvinCampos/features", glue = {"org.IrvinCampos.stepDefinitions"}, tags = "@DeletePlace"
+@CucumberOptions(features = "src/test/java/org/IrvinCampos/features",plugin = "json:target/jsonReports/Cucumber-report.json", glue = {"org.IrvinCampos.stepDefinitions"} //tags = "@DeletePlace"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
